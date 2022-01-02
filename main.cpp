3aeb35c1d8e657ed4d32655f6f457e607198fee1
Ruslan Guzey <guzeyruslan@yandex.ru>
2022-01-02 18:22:42 +0500

    Add multiple

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@ using namespace std;
 int main()
 {
     int a, b;
-    cout<<"Welcome"<< endl;
-    cin>> a >> b;
-    cout<< "Summa = "<< a+b;
-    cout<< "Raznost' = " << a-b; 
+    cout<<"Enter A: ";
+    cin>> a;
+    cout<<"Enter B: ";
+    cin>> b;
+    cout<< "Summa = "<< a+b << endl;
+    cout<< "Raznost' = " << a-b << endl;
+    cout<< "Multip = " << a*b << endl;
 }
